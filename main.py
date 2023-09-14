5d167d470605b22dcc91463fdb18b0614ef8929b
sneekywhite <99396336+sneekywhite@users.noreply.github.com>
2023-09-14 01:02:12 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 from fastapi import FastAPI, HTTPException, Depends, status
-from typing import Annotated
 import models
 from sqlalchemy.orm import Session
 from database import sessionLocal, engine
@@ -19,10 +18,10 @@ def get_db():
     finally:
         db.close()
 
-db_dependency = Annotated[Session, Depends(get_db)]
+
 
 @app.post("/api/", status_code=status.HTTP_201_CREATED, response_model= showPerson, tags=['person'])
-async def create_person(request:PersonBase, db: db_dependency):
+async def create_person(request:PersonBase, db:Session = Depends(get_db)):
     person = models.Person(**request.model_dump())
     db.add(person)
     db.commit()
@@ -31,7 +30,7 @@ async def create_person(request:PersonBase, db: db_dependency):
 
 
 @app.delete('/api/{user_id}/', status_code=status.HTTP_204_NO_CONTENT, tags=['deletePerson'])
-def deletePerson(user_id, db: db_dependency):
+def deletePerson(user_id, db:Session = Depends(get_db)):
     person = db.query(models.Person).filter(models.Person.user_id==user_id).first()
     if not person:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"person with the id {user_id} is not available")
@@ -42,7 +41,7 @@ def deletePerson(user_id, db: db_dependency):
 
 
 @app.put('/api/{user_id}/', status_code=status.HTTP_200_OK, tags=['updatePerson'])
-def update_person(user_id, request:showPerson, db:db_dependency):
+def update_person(user_id, request:showPerson, ):db:Session = Depends(get_db)
     db.query(models.Person).filter(models.Person.user_id == user_id).update({'name':request.name})
     db.commit()
     return 'updated successfully'
@@ -51,7 +50,7 @@ def update_person(user_id, request:showPerson, db:db_dependency):
 
 
 @app.get('/api/{user_id}/', status_code=status.HTTP_200_OK, response_model= showPerson, tags=['getPerson'])
-async def get_person(user_id, db:db_dependency):
+async def get_person(user_id, db:Session = Depends(get_db)):
     person = db.query(models.Person).filter(models.Person.user_id==user_id).first()
     if not person:
         #response.status_code = status.HTTP_404_NOT_FOUND
